9535de902acd7f85de4d995db1dfe0086d9a67e0
MisterOrange <misterorange006@gmail.com>
2025-04-15 14:59:51 +0200

    Initial project

---
+++ mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::Additem(std::string item) {
+    ui->listWidget->addItem(QString::fromStdString(item));
+}
